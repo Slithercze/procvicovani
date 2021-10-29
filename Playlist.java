a54eb46760e4e059e63c9761357afb91f408407e
Slithercze <Slithercze@gmail.com>
2021-10-29 19:16:00 +0200

    pridani dalsich cviceni

---
+++ Playlist.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+//Build Playlist.java with the best possible playlist of music using a Java ArrayList. Choose wisely.
+class Playlist {
+  
+  public static void main(String[] args) {
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+    desertIslandPlaylist.add("Diamonds");
+    desertIslandPlaylist.add("Despacito");
+    desertIslandPlaylist.add("Old Town Road");
+    desertIslandPlaylist.add("Dark Light");
+    desertIslandPlaylist.add("Maps");
+    desertIslandPlaylist.add("Rap God");
+    System.out.println(desertIslandPlaylist);
+    System.out.println(desertIslandPlaylist.size());
+    desertIslandPlaylist.remove("Rap God");
+    System.out.println(desertIslandPlaylist);
+    String a = desertIslandPlaylist.get(1); 
+    desertIslandPlaylist.set(1, "Dark Light");
+    desertIslandPlaylist.set(3, a);
+    System.out.println(desertIslandPlaylist);
+  }
+  
+}
